7ff11d8adb16cbcd8099a0a9145641f05c4778d7
baldaz <a.g.baldan@gmail.com>
2014-12-06 00:28:48 +0100

    0.1.1

--- test/bitterness.js
+++ test/bitterness.js
@@ -1,8 +1,18 @@
 var should = require('chai').should(),
-    bitterness = require('../index'),
+    bitterness = require('../index.js'),
     rager = bitterness.rager,
     tinseth = bitterness.tinseth;
 
+var hop_grams = 93,
+    alpha_acids = 6,
+    time = 90,
+    original_gravity = 1050,
+    batch_size = 25;
+
+bitterness = new bitterness(hop_grams, time, alpha_acids, batch_size, original_gravity);
+
 describe('#rager', function() {
-  rager()
+  it('calculate ibu', function() {
+    bitterness.rager().should.equal(71);
+  });
 });
