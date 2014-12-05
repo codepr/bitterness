9a773eb30263007bf00c7f4d22797d62c6a533e8
baldaz <a.g.baldan@gmail.com>
2014-12-05 21:05:43 +0100

    0.1.0

---
+++ test/bitterness.js
@@ -0,0 +1,8 @@
+var should = require('chai').should(),
+    bitterness = require('../index'),
+    rager = bitterness.rager,
+    tinseth = bitterness.tinseth;
+
+describe('#rager', function() {
+  rager()
+});
