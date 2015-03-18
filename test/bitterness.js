57f49616b0ee06b78e985c9867ff1cb83ad5f717
baldaz <a.g.baldan@gmail.com>
2015-03-18 18:24:11 +0100

    testint 0.1.2

--- test/bitterness.js
+++ test/bitterness.js
@@ -9,22 +9,36 @@ var hop_grams = 93,
     original_gravity = 1050,
     batch_size = 25;
 
-bitterness = new bitterness(hop_grams, time, alpha_acids, batch_size, original_gravity);
+bitterness_1 = new bitterness(hop_grams, time, alpha_acids, batch_size, original_gravity);
+var bitterness_2 = new bitterness(70, 80, 8.5, 35, 1057);
 
 describe('#rager', function() {
-  it('calculate ibu', function() {
-    bitterness.rager().should.equal(71);
-  });
-});
+    it('rager ibu calculation: 71', function() {
+        bitterness_1.rager().should.equal(71);
+    });
 
-describe('#tinseth', function() {
-    it('calculate ibu', function() {
-        bitterness.tinseth().should.equal(135);
+    it('rager ibu calculation: 54', function() {
+        bitterness_2.rager().should.equal(54);
     });
 });
 
+describe('#tinseth', function() {
+    it('tinseth ibu calculation: 135', function() {
+        bitterness_1.tinseth().should.equal(135);
+    });
+
+    it('tinseth ibu calculation: 108', function() {
+        bitterness_2.tinseth().should.equal(108);
+    });
+
+});
+
 describe('#average', function() {
-    it('calculate ibu', function() {
-        bitterness.average().should.equal(103);
+    it('average ibu calculation: 103', function() {
+        bitterness_1.average().should.equal(103);
+    });
+
+    it('average ibu calculation: 81', function() {
+        bitterness_2.average().should.equal(81);
     });
 });
